f84be7869948c34a439c9094bb6e5833989cd9ec
Alex Forrence <aforren1@jhu.edu>
2016-07-17 20:09:57 -0400

    More work, update Psychoobox due to typo

--- matlab/experiment/TimedResp.m
+++ matlab/experiment/TimedResp.m
@@ -25,21 +25,35 @@ classdef TimedResp < StateMachine
             s.win = PsychWindow(0, true, 'rect', s.consts.win_size,...
                                 'color', colour, ...
                                 'alpha_blending', true);
-            
+
+            % add audio
+            snd1 = wavread('misc/beep.wav');
+            snd2 = wavread('misc/smw_coin.wav');
+
+            s.aud = PsychAudio('mode', 9);
+            s.aud.AddSlave(1, 'channels', 2);
+            s.aud.AddSlave(2, 'channels', 2);
+
+            s.aud.FillBuffer([snd1; snd1]', 1);
+            s.aud.FillBuffer([snd2; snd2]', 2);
+
+            % add images
+            s.imgs = PsychTextures;
+            img_rect = .2 * s.win.Get('rect');
+            img_rect = CenterRectOnPoint(img_rect, s.win.center(1), s.win.center(2));
+            for ii = 1:length(name_array)
+                s.imgs.AddImage(img, s.win.pointer, ii,...
+                                'draw_rect', img_rect);
+
         end
 
         function Execute(s)
             done = false;
             stop_conditions = (GetSecs - s.ref_time > 4200) || done;
-            while 1
-                if stop_conditions
-                    break;
-                end
-
-
-
+            while ~stop_conditions
 
             end
+
         end
 
     end % end methods

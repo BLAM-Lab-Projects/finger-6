b273630102cbb7c3b068eaed97af7a5c560bb2b9
Alex Forrence <aforren1@jhu.edu>
2016-09-26 10:41:48 -0400

    cleanup

--- misc/tfiles/makeAllTFiles.m
+++ misc/tfiles/makeAllTFiles.m
@@ -1,5 +1,5 @@
 % make all target files for AVMA imaging experiment
-subjname = '001';
+subjname = '002';
 rng(sum(uint8(subjname)));
 
 tgt_path = ['C:/Users/fmri/Desktop/finger-6/misc/tfiles/',subjname,'/'];
